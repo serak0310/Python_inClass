51980872bd314650528a465f7b0cf6ba8f8f25ff
Minjeong Kim <serak0310@naver.com>
2022-10-14 13:09:23 +0900

    Ch04.기온그래프 그리기

--- Ch01/Basic.py
+++ Ch01/Basic.py
@@ -17,3 +17,14 @@ print(a)
 
 a = 0o12            # 8진수
 print(a)
+
+a = [1, 2, 3, 4]
+a[1:3]=['a','b','c']
+print(a)
+
+import pandas as pd
+data2 = [10, 20, 30, 40, 50]
+data3 = ["1반", "2반", "3반", "4반", "5반"]
+
+sr5 = pd.Series(data3, index=data2)
+print(sr5)
