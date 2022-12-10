398acd95520fbf108136e2fb605b324aeeee2ade
Minjeong Kim <serak0310@naver.com>
2022-12-11 02:09:41 +0900

    # 찾고 싶은 지역 이름 입력해서 항아리 모양 그래프 그리기 for문 1번, 2번 도는 방식

--- FinalExam/Unit8-regionJar.py
+++ FinalExam/Unit8-regionJar.py
@@ -9,12 +9,21 @@ f = []
 
 name = input('궁금한 지역의 이름을 입력해주세요: ')
 
+# for문 두 번 도는 방식
+# for row in data:
+#     if name in row[0]:
+#         for i in row[3:104]:
+#             m.append(-int(i))
+#         for i in row[106:]:
+#             f.append(int(i))
+
+# for문 한 번 도는 방식
 for row in data:
     if name in row[0]:
-        for i in row[3:104]:
-            m.append(-int(i))
-        for i in row[106:]:
-            f.append(int(i))
+        for i in range(101):
+            m.append(-int(row[i+3]))
+            f.append(int(row[-(i+1)]))
+f.reverse()
 
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
