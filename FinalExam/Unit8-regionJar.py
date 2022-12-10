3125733cb52b61def411689b2a29e0ce63a08937
Minjeong Kim <serak0310@naver.com>
2022-12-11 01:57:14 +0900

    Unit9. 찾고 싶은 지역 이름 입력해서 항아리 모양 그래프 그리기

--- FinalExam/Unit8-regionJar.py
+++ FinalExam/Unit8-regionJar.py
@@ -4,10 +4,24 @@ import csv
 f = open('gender.csv')
 data = csv.reader(f)
 
-m=[]
-f=[]
+m = []
+f = []
 
-# name = input('찾고 싶은 지역의 이름을 알려주세요: ')
-# for row in data:
-#     if name in row[0]:
+name = input('궁금한 지역의 이름을 입력해주세요: ')
 
+for row in data:
+    if name in row[0]:
+        for i in row[3:104]:
+            m.append(-int(i))
+        for i in row[106:]:
+            f.append(int(i))
+
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+plt.rc('font', family='Malgun Gothic')
+plt.rcParams['axes.unicode_minus'] = False
+plt.barh(range(101), m, label='남성')
+plt.barh(range(101), f, label='여성')
+plt.title(name+' 지역의 인구 성별 어쩌구')
+plt.legend()
+plt.show()
