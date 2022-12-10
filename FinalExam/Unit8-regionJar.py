71e2b828db1370f8063bbcdeb688fb752fce9a6a
Minjeong Kim <serak0310@naver.com>
2022-12-10 12:43:23 +0900

    Unit7,8 지역 그래프 그리기

---
+++ FinalExam/Unit8-regionJar.py
@@ -0,0 +1,13 @@
+# 찾고 싶은 지역 이름 입력해서 항아리 모양 그래프 그리기
+
+import csv
+f = open('gender.csv')
+data = csv.reader(f)
+
+m=[]
+f=[]
+
+# name = input('찾고 싶은 지역의 이름을 알려주세요: ')
+# for row in data:
+#     if name in row[0]:
+
