7adb07154b034e06cb31af8a34a6a10dccbc764f
Minjeong Kim <serak0310@naver.com>
2022-12-12 03:59:12 +0900

    혈액형 비율 파이 차트로 표현하기

--- FinalExam/Unit9-pie.py
+++ FinalExam/Unit9-pie.py
@@ -1,13 +1,12 @@
 # 혈액형 비율 파이 차트로 표현하기
 
 import matplotlib.pyplot as plt
-plt.rc('font', family='Malgun Gothic')
 
-scale = [2441, 2312, 1031, 1233]
-label = ['A', 'B', 'O', 'AN']
+size = [2441, 2312, 1031, 1233]
 color = ['darkmagenta', 'deeppink', 'hotpink', 'pink']
+label = ['A', 'B', 'O', 'AB']
 
 plt.axis('equal')
-plt.pie(scale, labels=label, autopct='%.1f%%', colors=color, explode=(0,0,0.1,0))
+plt.pie(size, labels=label, colors=color, autopct='%.1f%%', explode=(0,0,0.1,0))
 plt.legend()
 plt.show()
